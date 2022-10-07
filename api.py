b92f6cab4a677e010c80324fd1cff6e9ace496c6
Jason Garvin <66984922+Unfayr@users.noreply.github.com>
2022-10-06 18:47:23 -0600

    Add methods, functionality to apiController class
    Include means of converting a given id to a name, or a name to an id,
    for increased flexibility in how the API is called.
    
    Store additional information in an internal dictionary within the
    apiController class to retain more information and better handle
    the API call.

--- api.py
+++ api.py
@@ -16,60 +16,110 @@ class apiController:
     """An object that manages the connection between pokeapi
     (https://pokeapi.co/) and the running application."""
 
-    def __init__(self, api_address=None, cache_path="cache.json"):
+    def __init__(
+        self, endpoint, resource_type, name_or_id, cache_path="cache.json"
+        ):
         """Initializes the apiController with default uri
         to enable HTTP requests to the API source."""
+        _name, _id = self.convert_name_or_id(endpoint, name_or_id)
+        url = self._build_api_url(endpoint, resource_type, _name)
+        self.__dict__ = {
+            "name": _name,
+            "type": resource_type,
+            "endpoint": endpoint,
+            "id": _id,
+            "url": url}
+
         self.cache_path = cache_path
-        self.address = api_address
         self.resources = {}
 
-    def _build_api_url(self, endpoint, resource_id=None, subresource=None):
+        self.loaded = False
+
+    def _build_api_url(self, endpoint, resource_id, subresource):
         """Defines the full URL for the HTTP request"""
-        url = "/".join((endpoint, resource_id, subresource))
-        self.address = url
+        return "/".join((endpoint, resource_id, subresource))
+
+    def _convert_id_to_name(self, endpoint, id_):
+        """Takes the endpoint and the resource id, then
+        returns the resource name as a str"""
+        url = self._build_api_url(endpoint, self.__dict__["type"], id_)
+        resource_data = self.get_data(url)[url]
+
+        return resource_data.get("name", str(id_))
+
+    def _convert_name_to_id(self, endpoint, name):
+        """Takes the endpoint and the resource name, then
+        returns the resource id as an int"""
+        url = self._build_api_url(endpoint, self.__dict__["type"], name)
+        resource_data = self.get_data(url)[url]
+
+        return resource_data.get("id")
 
     def cache_resources(self):
         """Save the received resources from API call to JSON file"""
         with open(self.cache_path, "r+", encoding="utf-8") as file:
             file_data = json.load(file)
 
-            # TODO fix this to work with resources being its own dictionary
-            # Where the address is already the key to the resources
-            file_data[self.address] = self.resources[self.address]
+            # Iterate through saved URLs and store content with existing
+            # data from cache file with key as URL if dict has data
+            for (key, value) in self.resources.items():
+                if value is not None:
+                    file_data[key] = value
 
             file.seek(0)
             json.dump(file_data, file)
 
-    def get_data(self):
-        """Tries to retrieve data from the cache in case it already exists."""
+    def convert_name_or_id(self, endpoint, name_or_id):
+        """Converts a name to an ID or an ID to a name,
+        depending on type.
+
+        Assumes ID is int and name is str."""
+        if isinstance(name_or_id, int):
+            id_ = name_or_id
+            name = self._convert_id_to_name(endpoint, id_)
+        elif isinstance(name_or_id, str):
+            name = name_or_id
+            id_ = self._convert_name_to_id(endpoint, name)
+        else:
+            raise ValueError(f"'{name_or_id}' could not be converted")
+
+        return name, id_
+
+    def get_data(self, url=None):
+        """Tries to retrieve data from the cache in case it already exists.
+        Then calls _get_resource() to send GET request to API otherwise.
+
+        Retrieved data gets saved to self.resources as dict with url as key."""
+        if url is None:
+            url = self.__dict__["url"]
+
         try:
             with open(API_CACHE, "r+", encoding="utf-8") as cache:
                 data = json.load(cache)
 
-            if self.address in data.keys():
-                return data[self.address]
+            if url in data.keys():
+                return {url: data[url]}
 
         except FileNotFoundError:
             pass
 
-        data = self.get_resource()
+        data = self._get_resource()
         self.cache_resources()
 
         return data
 
-
-    def get_resource(self, timeout=10):
+    def _get_resource(self, timeout=10):
         """Sends a GET request to the API to receive the needed
         resource and saves it as a dict object before returning it.
 
-        Returns a dictionary of JSON-formatted data."""
+        Retrieved data gets saved to self.resources as dict with url as key."""
+        resource_url = self.__dict__["url"]
         try:
-            response = requests.get(self.address, timeout)
+            response = requests.get(resource_url, timeout)
             response.raise_for_status()
 
-            self.resources[self.address] = response.json()
-
-            return self.resources
+            self.resources[resource_url] = response.json()
+            return {resource_url: response.json()}
         except requests.exceptions.HTTPError as errh:
             print(errh)
         except requests.exceptions.ConnectionError as errc:
@@ -79,8 +129,4 @@ class apiController:
         except requests.exceptions.RequestException as err:
             print(err)
 
-        return {}
-
-    def set_address(self, address):
-        """Change the stored address."""
-        self.address = address
+        return {resource_url: None}
