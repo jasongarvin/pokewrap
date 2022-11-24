2c1f7bb0e9a85eb4b91fc27f8c3b6b403caf9072
Jason Garvin <wearesilence@gmail.com>
2022-11-24 10:18:45 -0700

    Expand Pokemon class in wrappers
    Bring all relevant categories from parsed JSON response dict object
    into attributes of the Pokemon wrapper class for ease of access.

--- pokewrap/wrappers.py
+++ pokewrap/wrappers.py
@@ -25,9 +25,34 @@ class Pokemon:
             resource_type="pokemon",
             name_or_id=name_or_id
         )
+
+        self.dict = self._build_dict()
+
+        self.abilities = self.dict.get("abilities", {})
+        self.base_exp = self.dict.get("base_experience", {})
+        self.encounters = self.dict.get("location_area_encounters", {})
+        self.forms = self.dict.get("forms", {})
+        self.game_indices = self.dict.get("game_indices", {})
+        self.height = self.dict.get("height", {})
+        self.held_items = self.dict.get("held_items", {})
+
         self.id = self.contents.id
+
+        self.is_default = self.dict.get("is_default", {})
+        self.moves = self.dict.get("moves", {})
+
         self.name = self.contents.name
+
+        self.order = self.dict.get("order", {})
+        self.past_types = self.dict.get("past_types", {})
+        self.species = self.dict.get("species", {})
+        self.sprites = self.dict.get("sprites", {})
+        self.stats = self.dict.get("stats", {})
+        self.types = self.dict.get("types", {})
+
         self.url = self.contents.url
+
+        self.weight = self.dict.get("weight", {})
 
     def __str__(self):
         """The string representation of the object"""
@@ -36,3 +61,18 @@ class Pokemon:
     def __repr__(self):
         """Developer representation of the object"""
         return f"<{self.name} #{self.id} at {self.url}>"
+
+    def _build_dict(self):
+        """Returns a dictionary object from further inside
+        the nested dict structure returned from the API call
+        """
+        # TODO make this less hacky
+        big_key = [key for key in self.contents.resources.keys()][0]
+        new_dict = {key: self.contents.resources[big_key][key]
+                    for key in self.contents.resources[big_key].keys()}
+
+        return new_dict
+
+
+if __name__ == "__main__":
+    test = Pokemon("gengar")
